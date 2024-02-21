95d36dc7c556ce0f56e88b4b1f669233dcfbd3ca
automationstubs-m360 <160709263+automationstubs-m360@users.noreply.github.com>
2024-02-21 15:43:34 +0530

    Add files via upload

--- src/main/java/com/m360/automation/intstubs/IntStubsApplication.java
+++ src/main/java/com/m360/automation/intstubs/IntStubsApplication.java
@@ -3,14 +3,22 @@ package com.m360.automation.intstubs;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
 public class IntStubsApplication {
+
+
+	
+	private RestTemplate restTemplate = new RestTemplate();
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntStubsApplication.class, args);
@@ -18,7 +26,8 @@ public class IntStubsApplication {
 
 	@Scheduled(cron = "0 */3 * ? * *")
 	public void runEvey5Minutes() {
-		System.out.println("Current time is :: " + LocalDateTime.now());
+		ResponseEntity<Object> response = restTemplate.getForEntity("https://int-stubs.onrender.com/api/intstubs/userdata", Object.class);
+		System.out.println("Response from int-stubs: " + response.getBody());
 	}
 
 }
