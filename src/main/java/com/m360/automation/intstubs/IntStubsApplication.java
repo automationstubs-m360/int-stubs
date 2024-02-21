0747885d0460fed3e80cd7c1ce1aae818ca09679
automationstubs-m360 <160709263+automationstubs-m360@users.noreply.github.com>
2024-02-21 14:19:11 +0530

    Add files via upload

--- src/main/java/com/m360/automation/intstubs/IntStubsApplication.java
+++ src/main/java/com/m360/automation/intstubs/IntStubsApplication.java
@@ -1,13 +1,24 @@
 package com.m360.automation.intstubs;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class IntStubsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntStubsApplication.class, args);
+	}
+
+	@Scheduled(cron = "0 */3 * ? * *")
+	public void runEvey5Minutes() {
+		System.out.println("Current time is :: " + LocalDateTime.now());
 	}
 
 }
