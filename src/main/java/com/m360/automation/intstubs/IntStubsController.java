7857650821fd7429778d563f4faf62ec202dc777
automationstubs-m360 <160709263+automationstubs-m360@users.noreply.github.com>
2024-02-21 12:39:40 +0530

    Add files via upload

---
+++ src/main/java/com/m360/automation/intstubs/IntStubsController.java
@@ -0,0 +1,19 @@
+package com.m360.automation.intstubs;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/api/intstubs")
+public class IntStubsController {
+
+    @GetMapping("/userdata")        
+    public Map<String,Object> getResponse(){
+        Map<String, Object> response  = Map.of("string", "srinivas!", "number", 23, "boolean", true);
+        return response;
+    }
+    
+}
