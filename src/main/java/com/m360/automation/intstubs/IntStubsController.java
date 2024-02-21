a1d931c887d055e58cc593558e5c86f1d705fb5c
automationstubs-m360 <160709263+automationstubs-m360@users.noreply.github.com>
2024-02-21 14:18:37 +0530

    Add files via upload

--- src/main/java/com/m360/automation/intstubs/IntStubsController.java
+++ src/main/java/com/m360/automation/intstubs/IntStubsController.java
@@ -1,8 +1,5 @@
 package com.m360.automation.intstubs;
 import java.util.Map;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api/intstubs")
 public class IntStubsController {
 
-  @GetMapping("/userdata")
-  public Map<String,Object> getResponse(){
-    Map<String, Object> response  = Map.of("string", "srinivas!", "number", 23, "boolean", true);
-    return response;
-  }
-
-  @PostMapping("/requestdata")
-  public Map<String,Object> sendRequest(@RequestBody  Map<String,Object> data){
-    Map<String, Object> response  = Map.of("string", "srinivas!", "number", 2333, "boolean", true);
-    return data;
-  }
+    @GetMapping("/userdata")        
+    public Map<String,Object> getResponse(){
+        Map<String, Object> response  = Map.of("string", "srinivas!", "number", 23, "boolean", true);
+        return response;
+    }
+    
 }
